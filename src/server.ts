c5c40897b71da10d075e58f7cc12988f60e845ae
maor <maorgithub@gmail.com>
2024-01-02 15:03:18 +0200

    #Added unclear git side effect

--- src/server.ts
+++ src/server.ts
@@ -4,26 +4,27 @@ import bodyParser from "body-parser";
 import  express  from "express";
 import cors from "cors";
 import dotenv from 'dotenv';
-import { userRouter } from '../routes/user'
-import { productRouter } from '../routes/product'
+import { userRouter } from './routes/user';
+import { productRouter } from './routes/product';
 dotenv.config()
 
 
 const URI = process.env.MONGOOSE_URI || 'mongodb+srv://maornetzer9:Maor013254777@maor.quyl8kx.mongodb.net/E-Commerce';
-const BACKEND_PORT = process.env.BACKEND_PORT || 4200;
+const BACKEND_PORT = process.env.PORT || 4200;
 const FRONTEND_PORT = process.env.FRONTEND_PORT || 3000;
 const app = express()
 
-const corsOptions = 
-{
-    origin  : process.env.FRONTEND_ORIGIN,
-    method  : 'GET, HEAD, PUT, PATCH, POST, DELETE',
-}
+// const corsOptions = 
+// {
+//     origin  : process.env.FRONTEND_ORIGIN,
+//     method  : 'GET, HEAD, PUT, PATCH, POST, DELETE',
+// }
 
-  app.use(cors(corsOptions))
-  app.use(bodyParser.json());
-  app.use('/user', userRouter);
-  app.use('/product', productRouter);
+//   app.use(cors(corsOptions))
+app.use( cors( ) );
+app.use(bodyParser.json());
+app.use('/user', userRouter);
+app.use('/product', productRouter);
 
   
 mongoose.connect(URI)
